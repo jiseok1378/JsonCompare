0309db9c41068dc80efad7d4036c01e7a68c0fbc
jiseok <jiseok1378@gmail.com>
2021-08-13 13:33:29 +0900

    add check debug

--- runModule.js
+++ runModule.js
@@ -1,13 +1,17 @@
 
 const fs =  require('fs');
 const os = require('os')
+const util = require('util')
 const path = require('path')
 const chalk = require('chalk')
 const log = console.log;
 
 const langFileList = Object.values(JSON.parse(fs.readFileSync('./targetFileList.json')))
 const langFileKey = Object.keys(JSON.parse(fs.readFileSync('./targetFileList.json')))
-
+const objectConsoleOption =  {
+    showHidden:true,
+    depth:2
+}
 const checkFlag = {
     TARGET : 'TARGET',
     CHECK : "CHECK",
@@ -76,35 +80,41 @@ const switchTargetFile = (files,index) => {
     return files;
 }
 
-const checkInner = (checkTagetFile, noneFile, fileName, preKey) => {
+const checkInner = (checkTargetFile, noneFile, fileName, preKey) => {
+    debugLog('checkInner', 
+            "checkTargetObject\n" +
+            util.inspect(checkTargetFile,objectConsoleOption) +"\n" +
+            "checkObject\n" + 
+            util.inspect(noneFile, objectConsoleOption))
+
     const pureFileName = fileName.split(path.sep)[fileName.split(path.sep).length - 1]
     if(noneFile != null){
-        const notFoundKeys = Object.keys(checkTagetFile).filter(x=>!Object.keys(noneFile).includes(x)); // 못찾은 key list
+        const notFoundKeys = Object.keys(checkTargetFile).filter(x=>!Object.keys(noneFile).includes(x)); // 못찾은 key list
         if(notFoundKeys.length != 0) notFoundKeys.forEach((notFoundKey)=>{
             findFlag = true
             log(`[${chalk.blue.bold(optionFlag.fullPath.flag ? fileName : pureFileName)}] MSSING KEY: ${preKey+ (preKey === ''? "" : ".") +notFoundKey}`)
             if(optionFlag.showingValue.flag)
-                log(`[${chalk.blue.bold(optionFlag.fullPath.flag ? fileName : pureFileName)}] MSSING VAL: ${checkTagetFile[notFoundKey]}\n`)
+                log(`[${chalk.blue.bold(optionFlag.fullPath.flag ? fileName : pureFileName)}] MSSING VAL: ${checkTargetFile[notFoundKey]}\n`)
         })  
     }
-    Object.keys(checkTagetFile).forEach((key)=>{
-        if(checkTagetFile[key] == undefined || checkTagetFile[key] == null || noneFile[key] == null){
+    Object.keys(checkTargetFile).forEach((key)=>{
+        if(checkTargetFile[key] == undefined || checkTargetFile[key] == null || noneFile[key] == null){
             return;
         } 
-        if(typeof(checkTagetFile[key]) === 'object') {
+        if(typeof(checkTargetFile[key]) === 'object') {
             const currentIndex = preKey === '' ?  key : ( preKey + "."+ key)
-            checkInner(checkTagetFile[key], noneFile[key], fileName, currentIndex)
+            checkInner(checkTargetFile[key], noneFile[key], fileName, currentIndex)
         }
     })
 
 }
 let findFlag = false; 
-const checkLanguage = (checkTagetFile, noneFiles, langKey) =>{
+const checkLanguage = (checkTargetFile, noneFiles, langKey) =>{
 
-    log(chalk.gray(`\n[Compare] TARGET: ${checkTagetFile.fileName}`))
+    log(chalk.gray(`\n[Compare] TARGET: ${checkTargetFile.fileName}`))
     for(let i = 0; i < noneFiles.length; i++){
         debugLog("checkLanguage", findFlag)
-        checkInner(checkTagetFile.json, noneFiles[i].json, noneFiles[i].fileName, '');
+        checkInner(checkTargetFile.json, noneFiles[i].json, noneFiles[i].fileName, '');
     }
     if(!findFlag) console.log(chalk.green("Message : No missing key found."))    
     findFlag = false
